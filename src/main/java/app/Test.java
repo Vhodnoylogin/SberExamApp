af97c2a2002750c3d355aa05c06b3071c12b9f91
Vhodnoylogin <kill.spama@gmail.com>
2022-06-16 22:33:55 +0300

    НЕ работает переместил рабочий класс в отдельную папку

---
+++ src/main/java/app/Test.java
@@ -0,0 +1,32 @@
+package app;
+
+public class Test {
+    public static void main(String[] args) {
+//        Data qql;
+//        try {
+//            qql = LookupOnAirportsFile.getDataById(1l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(2l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(3l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(7l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(10l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(15l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(17l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(19l);
+//            System.out.println(qql);
+//            qql = LookupOnAirportsFile.getDataById(240000l);
+//            System.out.println(qql);
+//        } catch (IOException | NoSuchRecordException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        System.out.println("HELLP");
+    }
+}

71983e3a4a176273999a836b6fb9a2bd6bba9671
Vhodnoylogin <kill.spama@gmail.com>
2022-06-16 21:42:46 +0300

    жалкие попытки все починить

--- src/main/java/com/exam/restserviceg/controllers/ControllerTask1.java
+++ src/main/java/com/exam/restserviceg/controllers/ControllerTask1.java
@@ -1,28 +1,25 @@
 package com.exam.restserviceg.controllers;
 
-import com.exam.restserviceg.logic.LookupOnAirportsFile;
-import com.exam.restserviceg.models.Data;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
-public class ControllerTask {
+public class ControllerTask1 {
     @ExceptionHandler(RuntimeException.class)
     @GetMapping("/airports")
-    public List<Data> getAllRows() throws IOException {
-        return LookupOnAirportsFile.getAllData();
+    public String getAllRows() throws IOException {
+        return "QWEQEQ";
     }
 
     @ExceptionHandler(RuntimeException.class)
     @GetMapping("/airports/{id}")
-    public Data getOneRow(
+    public String getOneRow(
             @PathVariable String id
     ) throws IOException {
-        return LookupOnAirportsFile.getDataById(Long.valueOf(id));
+        return id;
     }
 }

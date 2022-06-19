96bdee3e6fe5e1b4a38c3b6beba566bf368c5b74
Vhodnoylogin <kill.spama@gmail.com>
2022-06-20 00:00:06 +0300

    пытаемся делать мульти-проект

---
+++ client/src/main/java/com/exam/restservice/client/Client.java
@@ -0,0 +1,42 @@
+package com.exam.restservice.client;
+
+import com.exam.restserviceg.models.Greeting;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class Client {
+
+    static final String URL_RUN = "http://localhost:8080/";
+
+    public static void main(String[] args) {
+
+        // HttpHeaders
+        HttpHeaders headers = new HttpHeaders();
+
+//        headers.setAccept(List.of(MediaType.APPLICATION_XML));
+        // Request to return XML format
+//        headers.setContentType(MediaType.APPLICATION_XML);
+//        headers.set("my_other_key", "my_other_value");
+
+        // HttpEntity<Employee[]>: To get result as Employee[].
+        HttpEntity<Greeting> entity = new HttpEntity<>(headers);
+
+        // RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Send request with GET method, and Headers.
+        ResponseEntity<Greeting> response = restTemplate.exchange(URL_RUN,
+                HttpMethod.GET, entity, Greeting.class);
+
+        HttpStatus statusCode = response.getStatusCode();
+        System.out.println("Response Satus Code: " + statusCode);
+
+        // Status Code: 200
+        if (statusCode == HttpStatus.OK) {
+            // Response Body Data
+            Greeting res = response.getBody();
+            System.out.println(res);
+        }
+
+    }
+}

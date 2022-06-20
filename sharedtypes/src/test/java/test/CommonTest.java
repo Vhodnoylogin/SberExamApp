7e342877974cfb588012020b77c219c89c1cfca1
Vhodnoylogin <kill.spama@gmail.com>
2022-06-21 02:23:26 +0300

    Сделал тесты. Думал, что там есть ошибка. А ошибки нет.

---
+++ sharedtypes/src/test/java/test/CommonTest.java
@@ -0,0 +1,46 @@
+package test;
+
+
+import help.MyTimestamp;
+import org.junit.jupiter.api.Assertions;
+
+import java.time.LocalDateTime;
+
+public class CommonTest {
+    @org.junit.jupiter.api.Test
+    public void test_string_to_timestamp_1() {
+        String t = "2022-06-21T02:10:57.2066531";
+        try {
+            LocalDateTime qql = MyTimestamp.parse(t);
+            System.out.println(qql);
+            Assertions.assertTrue(true);
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test_string_to_timestamp_2() {
+        String t = "2022-06-21T01:07:08.8060455";
+        try {
+            LocalDateTime qql = MyTimestamp.parse(t);
+            System.out.println(qql);
+            Assertions.assertTrue(true);
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test_string_to_timestamp_3_fail() {
+        String t = "2022-06-2 01:07:08.8060455";
+        try {
+            LocalDateTime qql = MyTimestamp.parse(t);
+            System.out.println(qql);
+            Assertions.assertNull(qql);
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+}

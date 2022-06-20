d2c38757b49966ecd1ffa0a6f9f87049fe27a443
Vhodnoylogin <kill.spama@gmail.com>
2022-06-20 22:01:59 +0300

    делаем клиент

---
+++ client/src/main/java/com/exam/restservice/client/RequestBuilder.java
@@ -0,0 +1,10 @@
+package com.exam.restservice.client;
+
+public class RequestBuilder {
+    protected static final String URL_BASE = "http://localhost:8080";
+    protected static final String URL_RUN = URL_BASE + "/";
+    protected static final String URL_ANY_RUN = URL_BASE + "/greeting";
+    protected static final String URL_AIRPORTS = URL_BASE + "/airports/get";
+
+
+}

0ee90bb561e5eed47aef36d6255108486acb7cbd
Vhodnoylogin <kill.spama@gmail.com>
2022-06-26 16:13:25 +0300

    synchronized

--- server/src/main/java/restserviceg/logic/LookupOnAirportsFile.java
+++ server/src/main/java/restserviceg/logic/LookupOnAirportsFile.java
@@ -8,46 +8,51 @@ import restserviceg.logic.exceptions.RecordNotFoundException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LookupOnAirportsFile {
     protected static final String FILE_AIRPORT_PATH = "/data/airports.dat";
 
     protected static Map<Long, Airport> cashedFile;
+    protected final static Object mutex = new Object();
 
     protected static Map<Long, Airport> getFromFile() throws IOException {
-        if (cashedFile == null) {
-            File f = new ClassPathResource(
-                    FILE_AIRPORT_PATH
-                    , LookupOnAirportsFile.class.getClassLoader()
-            ).getFile();
-            cashedFile = new CsvToBeanBuilder<Airport>(new FileReader(f))
-                    .withType(Airport.class)
-                    .withIgnoreEmptyLine(true)
-                    .build()
-                    .stream()
-                    .filter(x -> x.getId() != null)
-                    .collect(Collectors.toMap(
-                            Airport::getId
-                            , x -> x)
-                    );
-        }
+        File f = new ClassPathResource(
+                FILE_AIRPORT_PATH
+                , LookupOnAirportsFile.class.getClassLoader()
+        ).getFile();
+        return Collections.unmodifiableMap(
+                new CsvToBeanBuilder<Airport>(new FileReader(f))
+                        .withType(Airport.class)
+                        .withIgnoreEmptyLine(true)
+                        .build()
+                        .stream()
+                        .filter(x -> x.getId() != null)
+                        .collect(Collectors.toMap(
+                                Airport::getId
+                                , x -> x)
+                        )
+        );
+    }
 
+    protected static Map<Long, Airport> getMapData() throws IOException {
+        synchronized (mutex) {
+            if (cashedFile == null) {
+                cashedFile = getFromFile();
+            }
+        }
         return cashedFile;
     }
 
     public static Airport getDataById(Long id) throws IOException {
         return Optional
-                .ofNullable(getFromFile().get(id))
+                .ofNullable(getMapData().get(id))
                 .orElseThrow(() -> new RecordNotFoundException(id));
 
     }
 
     public static List<Airport> getAllData() throws IOException {
-        return new ArrayList<>(getFromFile().values());
+        return new ArrayList<>(getMapData().values());
     }
 }
